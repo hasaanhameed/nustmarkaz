c70609831f0d9aa27ecd2a8f9210b6a755deec36
Hasaan Hameed <hasaanhameed52@gmail.com>
2026-01-22 23:08:19 +0500

    Login Sign UP issue fixed

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from routers import user, authentication
 from database import engine, Base
-
+from fastapi.middleware.cors import CORSMiddleware
 # Create all tables in the database
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+       CORSMiddleware,
+       allow_origins=["*"],  
+       allow_credentials=True,
+       allow_methods=["*"],
+       allow_headers=["*"],
+   )
 
 # Include the user router
 app.include_router(user.router)
--- backend/routers/user.py
+++ backend/routers/user.py
@@ -3,20 +3,19 @@ from sqlalchemy.orm import Session
 from models.user import User
 from schemas.user import UserCreate
 from database import get_db
-from hashing import Hash  # Import the Hash class
+from hashing import Hash  
 
 router = APIRouter(prefix="/users", tags=["users"])
 
 @router.post("/")
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
-    # Hash the password before saving it to the database
     hashed_password = Hash.bcrypt(user.password)
     
     db_user = User(
         username=user.username,
         email=user.email,
         department=user.department,
-        password=hashed_password  # Store the hashed password
+        password=hashed_password  
     )
     
     db.add(db_user)
